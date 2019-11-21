32ff6e4e8c9aeba553fd2d8815b44ab2324156a2
tarak0-2000 <58031809+tarak0-2000@users.noreply.github.com>
2019-11-21 20:36:38 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+$(function() {
+	// スクロールしたときに実行
+	$(window).scroll(function () {
+		// 目的のスクロール量を設定(px)
+		var TargetPos = 350;
+		// 現在のスクロール位置を取得
+		var ScrollPos = $(window).scrollTop();
+		// 現在位置が目的のスクロール量に達しているかどうかを判断
+		if( ScrollPos >= TargetPos ) {
+			// 達していれば表示
+			$("#top-button").fadeIn();
+		}
+		else {
+			// 達していなければ非表示
+			$("#top-button").fadeOut();
+		}
+	});
+
+	// ページ内スムーススクロール
+	$('a[href^="#"]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = $(target).offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+
+		return false;
+	});
+
+	// 詳細表示制御
+	$('.frame').click(function(){
+		var thisId = $(this).attr("id");
+		var targetId = "#" + thisId + "-section";
+		var targetHide = $(targetId).css('display') == 'none' ? true : false;
+
+
+		if( targetHide ) {
+			// ターゲットが非表示ならスライドで表示する
+			$(".details-area").slideUp(1000);
+			$(targetId).slideDown(1000);
+			setTimeout(function(){
+				// 表示されたターゲットへ移動する
+				var position = $(targetId).offset().top;
+				if( position ) {
+					$("html, body").animate({scrollTop:position}, 500, "swing");
+				}
+			},800);
+		}else{
+			// ターゲットが表示されているならそこへ移動する
+			var position = $(targetId).offset().top;
+			$("html, body").animate({scrollTop:position}, 500, "swing");
+		}
+
+		return false;
+	});
+
+});
